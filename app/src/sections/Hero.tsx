e5ed8f3cd096ce8010aaa8e751e78485755ef41e
K.Rumesha Ishini Silva <ishinisilva19@gmail.com>
2026-01-29 09:44:08 +0530

    changes hero.tsx page my profil pic

--- app/src/sections/Hero.tsx
+++ app/src/sections/Hero.tsx
@@ -208,7 +208,7 @@ export default function Hero() {
                 <img
                   src="/1.jpeg"
                   alt="Kavirathna Rumesha Ishini Silva"
-                  className="w-full h-full object-cover"
+                  className="w-100 h-100 object-cover"
                 />
                 {/* Overlay gradient */}
                 <div className="absolute inset-0 bg-gradient-to-t from-black/40 via-transparent to-transparent" />
